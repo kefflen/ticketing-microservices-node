e238a0189a4f1846060c3b90da3849c563ff8843
kefflen <kefflenjava@hotmail.com>
2023-11-15 18:12:44 -0300

    Add user schema model.

---
+++ auth/src/models/userSchema.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose"
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type:String,
+    required: true
+  },
+})
+
+const UserModel = model('User', userSchema)
+
+export default UserModel
