6d7b2a0f16bc6537e33f5bff449e3a4f04c1f4e7
kefflen <kefflenjava@hotmail.com>
2023-11-16 09:02:27 -0300

    Remove password field and rename _id to id in userSchema toJSON

--- auth/src/models/userSchema.ts
+++ auth/src/models/userSchema.ts
@@ -11,6 +11,14 @@ const userSchema = new Schema({
     type:String,
     required: true
   },
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      delete ret.password
+      ret.id = ret._id
+      delete ret._id
+    }
+  }
 })
 
 const UserModel = model('User', userSchema)
