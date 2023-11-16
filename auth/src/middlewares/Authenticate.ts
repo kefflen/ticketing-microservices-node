ef62508c9789be7997bdf35f316e1bdb25d6c138
kefflen <kefflenjava@hotmail.com>
2023-11-16 09:20:51 -0300

    Add authentication middleware and payload interface

---
+++ auth/src/middlewares/Authenticate.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+function Authenticate(req: Request, res: Response, next: NextFunction) {
+  const [bearer, token] = req.headers.authorization?.split(' ') || []
+  if (!bearer || !token) {
+    return res.status(401).send('Unauthorized')
+  }
+
+  if (bearer !== 'Bearer') {
+    return res.status(401).send('Unauthorized')
+  }
+
+  if (!process.env.SECRET_KEY) {
+    throw new Error('SECRET_KEY must be defined')
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.SECRET_KEY)
+    req.payload = payload as { id: string, email: string }
+  } catch (err) {
+    return res.status(401).send('Unauthorized')
+  }
+}
