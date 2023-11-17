0b0a918f018cd9ade1888ecaa2bf07cfde3d4a96
kefflen <kefflenjava@hotmail.com>
2023-11-17 19:45:15 -0300

    Add ticket validation schema

--- tickets/src/routes.ts
+++ tickets/src/routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express"
+import zod from 'zod'
 import { authenticated } from "./middlewares/authenticated"
-
+//Captalize title
 const routes = Router()
+const ticketSchema = zod.object({
+  title: zod.string().trim().min(1).max(100),
+  price: zod.number().gt(0)
+})
 
 routes.post('/', authenticated, (req, res) => {
+  const { title, price } = ticketSchema.parse(req.body)
+
   res.sendStatus(200)
 })
 
