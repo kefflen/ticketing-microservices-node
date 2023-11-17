946c1785d9a2264d0de881ebcb239ee0e8ca7d07
kefflen <kefflenjava@hotmail.com>
2023-11-17 19:48:41 -0300

    Add ticket model schema

---
+++ tickets/src/models/ticketModel.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const ticketSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id
+      delete ret._id
+    }
+  }
+})
+
+const TicketModel = model('Ticket', ticketSchema)
+
+export default TicketModel
