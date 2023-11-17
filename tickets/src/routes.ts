07c9382c8829e80cbc8f9596c82f41f202be82e0
kefflen <kefflenjava@hotmail.com>
2023-11-17 19:55:00 -0300

    Add ticket creation functionality

--- tickets/src/routes.ts
+++ tickets/src/routes.ts
@@ -1,17 +1,21 @@
 import { Router } from "express"
 import zod from 'zod'
 import { authenticated } from "./middlewares/authenticated"
-//Captalize title
+import TicketModel from "./models/ticketModel"
+
 const routes = Router()
 const ticketSchema = zod.object({
   title: zod.string().trim().min(1).max(100),
   price: zod.number().gt(0)
 })
 
-routes.post('/', authenticated, (req, res) => {
+routes.post('/', authenticated, async (req, res) => {
   const { title, price } = ticketSchema.parse(req.body)
 
-  res.sendStatus(200)
+  const ticket = new TicketModel({ title, price })
+  await ticket.save()
+
+  res.status(201).json(ticket)
 })
 
 export default routes
