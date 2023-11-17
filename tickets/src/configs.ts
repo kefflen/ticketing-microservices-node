eba2b01b4eff4a00304bd61f71f5120703b57067
kefflen <kefflenjava@hotmail.com>
2023-11-17 19:18:04 -0300

    Add Kubernetes deployment and service files for tickets and tickets-mongo

--- tickets/src/configs.ts
+++ tickets/src/configs.ts
@@ -1,13 +1,16 @@
 type configs = {
   mongoURI: string
+  authUrl: string
 }
 
 const localEnvConfigs: configs = {
-  mongoURI: 'mongodb://localhost:27017/auth'
+  mongoURI: 'mongodb://localhost:27017/auth',
+  authUrl: 'http://localhost:3000'
 }
 
 const kubernetesEnvConfigs: configs = {
-  mongoURI: 'mongodb://auth-mongo-srv:27017/auth'
+  mongoURI: 'mongodb://tickets-mongo-srv:27017/tickets',
+  authUrl: 'http://auth-srv:3000'
 }
 
 const configs: configs = process.env.ENV == 'KUBERNETES' ? kubernetesEnvConfigs : localEnvConfigs
