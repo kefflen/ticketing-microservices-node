851d17203ec13436c323932f45349bf7629d36bf
kefflen <kefflenjava@hotmail.com>
2023-11-17 19:38:34 -0300

    Add authenticated middleware and route for creating tickets

---
+++ tickets/src/middlewares/authenticated.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from 'express'
+import axios from 'axios'
+import configs from '../configs'
+type payload = {
+  id: string
+  email: string
+}
+
+export async function authenticated(req: Request, res: Response, next: NextFunction) {
+  const authUrl = configs.authUrl
+
+  const authorizationHeader = req.headers.authorization
+  if (authorizationHeader) {
+    return res.status(401).send('Unauthorized')
+  }
+
+  try {
+    const { data: payload } = await axios.get<payload>(authUrl + '/api/users/currentuser', {
+      headers: {
+        authorization: authorizationHeader
+      }
+    })
+    req.payload = payload as { id: string, email: string }
+  } catch (err: any) {
+    if (!!err.response && err.response.status == 401) {
+      return res.status(401).send('Unauthorized')
+    } else {
+      throw err
+    }
+  }
+
+  return next()
+}
--- tickets/src/routes.ts
+++ tickets/src/routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express"
+import { authenticated } from "./middlewares/authenticated"
 
 const routes = Router()
 
-
+routes.post('/', authenticated, (req, res) => {
+  res.sendStatus(200)
+})
 
 export default routes
