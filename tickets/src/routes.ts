4618fa8cf56f74df8d6281ed4fde682353d9bd95
kefflen <kefflenjava@hotmail.com>
2023-11-19 21:37:39 -0300

    Add routes for getting all tickets and updating a ticket

--- tickets/src/routes.ts
+++ tickets/src/routes.ts
@@ -28,4 +28,25 @@ routes.get('/:id', async (req, res) => {
   res.json(ticket)
 })
 
+routes.get('/', async (req, res) => {
+  const tickets = await TicketModel.find({})
+
+  res.json(tickets)
+})
+
+routes.put('/:id', authenticated, async (req, res) => {
+  const { title, price } = ticketSchema.parse(req.body)
+
+  const ticket = await TicketModel.findById(req.params.id)
+
+  if (!ticket) {
+    return res.status(404).send('Not found')
+  }
+
+  ticket.set({ title, price })
+  await ticket.save()
+
+  res.json(ticket)
+})
+
 export default routes
