7fa75448b7df5782e5e4789fbbda9ded52730aa7
kefflen <kefflenjava@hotmail.com>
2023-11-20 16:04:44 -0300

    Add signup route tests

--- auth/src/__test__/auth-routes.test.ts
+++ auth/src/__test__/auth-routes.test.ts
@@ -1,18 +1,101 @@
+import request from 'supertest'
+import UserModel from '../models/userSchema'
+import app from '../server'
+import mongoose from 'mongoose'
+import configs from '../configs'
 
 describe('Auth API test suite', () => {
-  describe('POST /api/users/signin', async () => {
+  let userCredentials: {
+    email: string
+    password: string
+  }
+
+  beforeAll(async () => {
+    try {
+      await mongoose.connect(configs.mongoURI)
+      console.log('Connected to MongoDB')
+    } catch (err) {
+      console.log(err)
+      return
+    }
+  })
+
+  beforeEach(async () => {
+    await UserModel.deleteMany({})
+    userCredentials = {
+      email: 'any@email.com',
+      password: 'any_password'
+    }
+  })
+  describe('POST /api/users/signup', () => {
+    it('should return 201 on successful signup', async () => {
+      console.log('Teste')
+      await request(app)
+        .post('/api/users/signup')
+        .send(userCredentials)
+        .expect(201)
+    })
+    
+    it('returns a 400 with an invalid email', async () => {
+      await request(app)
+        .post('/api/users/signup')
+        .send({
+          ...userCredentials,
+          email: 'invalid_email'
+        })
+        .expect(400)
+    })
+
+    it('returns a 400 with password with less than three characters', async () => {
+      await request(app)
+        .post('/api/users/signup')
+        .send({
+          ...userCredentials,
+          password: '123'
+        })
+        .expect(400)
+    })
+
+    it('returns a 400 with password with more than twenty characters', async () => {
+      await request(app)
+        .post('/api/users/signup')
+        .send({
+          ...userCredentials,
+          password: '123456789012345678901'
+        })
+        .expect(400)
+    })
+
+    it('returns a 400 with missing email and password', async () => {
+      await request(app)
+        .post('/api/users/signup')
+        .send({})
+        .expect(400)
+    })
+
+    it('disallows duplicate emails', async () => {
+      await request(app)
+        .post('/api/users/signup')
+        .send(userCredentials)
+        .expect(201)
+      
+      await request(app)
+        .post('/api/users/signup')
+        .send(userCredentials)
+        .expect(400)
+    })
+  })
+
+  describe('POST /api/users/signin', () => {
 
   })
 
-  describe('POST /api/users/signup', async () => {
 
+  describe('POST /api/users/signout', () => {
+    it.todo('should logout the user')
   })
 
-  describe('POST /api/users/signout', async () => {
-
-  })
-
-  describe('GET /api/users/currentuser', async () => {
+  describe('GET /api/users/currentuser', () => {
 
   })
 })
