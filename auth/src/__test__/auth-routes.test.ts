6431db6fa28996f81b62cc84ca74e9fb90016fa6
kefflen <kefflenjava@hotmail.com>
2023-11-20 15:42:00 -0300

    Add supertest for API testing

---
+++ auth/src/__test__/auth-routes.test.ts
@@ -0,0 +1,18 @@
+
+describe('Auth API test suite', () => {
+  describe('POST /api/users/signin', async () => {
+
+  })
+
+  describe('POST /api/users/signup', async () => {
+
+  })
+
+  describe('POST /api/users/signout', async () => {
+
+  })
+
+  describe('GET /api/users/currentuser', async () => {
+
+  })
+})
