7aba6ef9e0cf7cc1e0c329c99cfbe44580087ba3
kefflen <kefflenjava@hotmail.com>
2023-11-25 10:34:25 -0300

    Add KafkaService class and connect to Kafka broker

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,15 +2,26 @@ import './load-envs'
 import mongoose from 'mongoose'
 import configs from './configs'
 import app from './server'
+import { KafkaService } from '../KafkaService'
+
+export const kafkaService = new KafkaService()
 
 const start = async () => {
+  try {
+    kafkaService.connect()
+  } catch (err) {
+    console.log(err)
+    return
+  }
+
   try {
     await mongoose.connect(configs.mongoURI)
     console.log('Connected to MongoDB')
   } catch (err) {
     console.log(err)
     return
-  } 
+  }
+
   const PORT = process.env.PORT || 3000
   app.listen(
     PORT,
